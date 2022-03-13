ae4c58cf64612eb5b54b0013976c11d407c460fb
Inung4065 <kjsa4065@gmail.com>
2022-03-14 01:18:08 +0900

    Signed-off-by: Inung4065 <kjsa4065@gmail.com>

---
+++ code/ISSMINI/src/main/java/com/spring/vo/AccVo.java
@@ -0,0 +1,26 @@
+package com.spring.vo;
+
+public class AccVo {
+	private String accountId;
+	private String accountPassword;
+	private String accountMail;
+	
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	public String getAccountPassword() {
+		return accountPassword;
+	}
+	public void setAccountPassword(String accountPassword) {
+		this.accountPassword = accountPassword;
+	}
+	public String getAccountMail() {
+		return accountMail;
+	}
+	public void setAccountMail(String accountMail) {
+		this.accountMail = accountMail;
+	}
+}
