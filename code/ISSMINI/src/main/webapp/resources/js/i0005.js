48fca46b783b29994715dd8982cb6fe8e408fa52
kimchulho-iss <jolipon@naver.com>
2022-05-29 01:40:49 +0900

    commit

---
+++ code/ISSMINI/src/main/webapp/resources/js/i0005.js
@@ -0,0 +1,27 @@
+$( function(  ) {
+	$( "#btnCreate" ).click( function(  ) {
+
+		var textTitle=$( "#textTitleForCheck" ).val(  );
+		var textContents=$( "#textContentsForCheck" ).val(  );
+
+		if ( textTitle == "" ) {
+			var msg = document.getElementById("IME0004").value;
+			alert( msg );
+			$( "#textTitleForCheck" ).focus(  );
+			$( 'form' ).bind( 'submit', false );
+			
+			return;
+		}
+
+		if ( textContents == "" ) {
+			var msg = document.getElementById("IME0004").value;
+			alert( msg );
+			$( "#textContentsForCheck" ).focus(  );
+			$( 'form' ).bind( 'submit', false );
+		
+			return;
+		}
+
+		$( 'form' ).unbind(  );
+	} );
+} );
